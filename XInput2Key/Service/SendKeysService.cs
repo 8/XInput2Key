9984172cc8ecaffc38159230adbc79a9de2b9e86
mdk <mdk@lostindetails.com>
2017-03-31 08:45:11 +0200

    added draft of SendKeysService

---
+++ XInput2Key/Service/SendKeysService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace XInput2Key.Service
+{
+    public interface ISendKeysService
+    {
+        void SendKeys(string keys);
+    }
+
+    public class SendKeysService : ISendKeysService
+    {
+        private INPUT[] GetInputs(string keys)
+        {
+            return new[]
+            {
+                new INPUT
+                {
+                    type = InputType.KEYBOARD,
+                    U = new InputUnion
+                    {
+                        ki = new KEYBDINPUT
+                        {
+                            wScan = ScanCodeShort.KEY_H,
+                            wVk = VirtualKeyShort.KEY_H
+                        }
+                    }
+                },
+                new INPUT
+                {
+                    type = InputType.KEYBOARD,
+                    U = new InputUnion
+                    {
+                        ki = new KEYBDINPUT
+                        {
+                            wScan = ScanCodeShort.KEY_H,
+                            wVk = VirtualKeyShort.KEY_H,
+                            dwFlags = KEYEVENTF.KEYUP
+                        }
+                    }
+                }
+            };
+        }
+
+        public void SendKeys(string keys)
+        {
+            var pInputs = GetInputs(keys);
+
+            NativeApi.SendInput((uint)pInputs.Length, pInputs, INPUT.Size);
+        }
+    }
+}
