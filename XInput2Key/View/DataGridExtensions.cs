dc6547f52f537424c9cdd4d4f5d1ff962a4e5ab1
mdk <mdk@lostindetails.com>
2017-04-02 00:10:21 +0200

    started implementing ButtonMappingViewModel

---
+++ XInput2Key/View/DataGridExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XInput2Key.View
+{
+    public static class DataGridExtensions
+    {
+        #region DataContext
+
+        public static object GetColumnDataContext(DependencyObject obj)
+        {
+            return (object)obj.GetValue(ColumnDataContextProperty);
+        }
+
+        public static void SetColumnDataContext(DependencyObject obj, object value)
+        {
+            obj.SetValue(ColumnDataContextProperty, value);
+        }
+
+        public static readonly DependencyProperty ColumnDataContextProperty =
+            DependencyProperty.RegisterAttached("ColumnDataContext", typeof(object), typeof(DataGridExtensions), new PropertyMetadata(null, OnColumnDataContextChanged));
+
+        private static void OnColumnDataContextChanged(object owner, DependencyPropertyChangedEventArgs e)
+        {
+            UpdateColumnsDataContext(owner as DataGrid, e.NewValue);
+        }
+
+        private static void UpdateColumnsDataContext(DataGrid dataGrid, object dataContext)
+        {
+            if (dataGrid != null)
+            {
+                foreach (var c in dataGrid.Columns)
+                    c.SetValue(FrameworkElement.DataContextProperty, dataContext);
+
+                dataGrid.Columns.CollectionChanged += (o, e) => Columns_CollectionChanged(dataContext, e);
+            }
+        }
+
+        private static void Columns_CollectionChanged(object dataContext, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (var c in e.NewItems.OfType<DataGridColumn>())
+                    c.SetValue(FrameworkElement.DataContextProperty, dataContext);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ XInput2Key/ViewModel/ButtonMappingViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace XInput2Key.ViewModel
+{
+    using SharpDX.XInput;
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using XInput2Key.Factory;
+
+    public interface IButtonMappingViewModel
+    {
+    }
+
+    public class ButtonMappingItemViewModel
+    {
+        public InputKey Key { get; set; }
+        public GamepadKeyCode  Button { get; set; }
+    }
+
+    public class ButtonMappingViewModel : BasePropertyChanged, IButtonMappingViewModel
+    {
+        public IEnumerable<ButtonMappingItemViewModel> Items { get; private set; }
+
+        public IEnumerable<InputKey> AllKeys { get; private set; }
+
+        public ButtonMappingViewModel()
+        {
+            this.Items = GetItems();
+            this.AllKeys = (InputKey[])Enum.GetValues(typeof(InputKey));
+        }
+
+        private IEnumerable<ButtonMappingItemViewModel> GetItems()
+        {
+            return ((GamepadKeyCode[])Enum.GetValues(typeof(GamepadKeyCode)))
+                .Select(b => new ButtonMappingItemViewModel { Button = b, Key = InputKey.F1 })
+                .ToArray();
+        }
+    }
+}
