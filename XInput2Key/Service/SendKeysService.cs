01233f7a291686e3203844913fd6daefbdae8425
mdk <mdk@lostindetails.com>
2017-04-01 14:50:31 +0200

    removed commented out code

--- XInput2Key/Service/SendKeysService.cs
+++ XInput2Key/Service/SendKeysService.cs
@@ -7,45 +7,6 @@
 
     public class SendKeysService : ISendKeysService
     {
-        //private INPUT[] GetInputs(string keys)
-        //{
-        //    return new[]
-        //    {
-        //        new INPUT
-        //        {
-        //            type = InputType.KEYBOARD,
-        //            U = new InputUnion
-        //            {
-        //                ki = new KEYBDINPUT
-        //                {
-        //                    wScan = ScanCodeShort.KEY_H,
-        //                    wVk = VirtualKeyShort.KEY_H
-        //                }
-        //            }
-        //        },
-        //        new INPUT
-        //        {
-        //            type = InputType.KEYBOARD,
-        //            U = new InputUnion
-        //            {
-        //                ki = new KEYBDINPUT
-        //                {
-        //                    wScan = ScanCodeShort.KEY_H,
-        //                    wVk = VirtualKeyShort.KEY_H,
-        //                    dwFlags = KEYEVENTF.KEYUP
-        //                }
-        //            }
-        //        }
-        //    };
-        //}
-
-        //public void SendKeys(string keys)
-        //{
-            //var pInputs = GetInputs(keys);
-
-        //    NativeApi.SendInput((uint)pInputs.Length, pInputs, INPUT.Size);
-        //}
-
         public void SendKeys(INPUT[] inputs)
         {
             if (inputs != null && inputs.Length > 0)
